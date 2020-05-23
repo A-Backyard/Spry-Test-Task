df80d0cb920cf921efc9260a5d0ffeeed66827fc
A-Backyard <anton.backyard@gmail.com>
2020-05-23 18:24:44 +0300

    commit

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+const ios = require('@react-native-community/cli-platform-ios');
+const android = require('@react-native-community/cli-platform-android');
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets:
+    ['./assets/fonts'],
+};
